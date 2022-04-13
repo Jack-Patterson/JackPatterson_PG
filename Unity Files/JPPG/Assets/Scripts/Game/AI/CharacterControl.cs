ffe30712f7062417ab635eb116229ebd08d4c888
Jack-Patterson <72133977+Jack-Patterson@users.noreply.github.com>
2022-04-13 15:45:48 +0100

    Animals
    Started script to make animals for a food source job and added models

---
+++ Unity Files/JPPG/Assets/Scripts/Game/AI/CharacterControl.cs
@@ -0,0 +1,433 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterControl : MonoBehaviour
+{
+
+    float characterInventory;
+    float characterInventoryMax = 10;
+
+    ResourceManager.Resource resourceType/* = ResourcesManager.Resources.Stone*/;
+    HarvestableObject harvest;
+
+    StorageItem storage;
+    
+    IInteractable interactableObject;
+
+    public enum CharacterState {walkingToJob, idle, DoingJob, ReturningFromJob, ReturningToRestPosition, mining, practice, reading, storing, resting};
+    CharacterState currentState = CharacterState.idle;
+    CharacterState lastState = CharacterState.idle;
+
+    public enum Job { None, Fighter, Scholar, Miner, Lumberjack, Farmer }
+    [SerializeField]
+    Job job = Job.None;
+
+    // Getting Elements from this gameobject or others
+    NavMeshAgent agent;
+
+    Animator animator;
+    Rigidbody rigidBody;
+    CapsuleCollider capsule;
+
+    GameObject standPos;
+
+    // Obtained from Capsule Collider
+    float capsuleHeight;
+    Vector3 capsuleCenter;
+
+    // Mining Related
+    GameObject pickaxe;
+
+    // Weapons Related
+    GameObject sword;
+    GameObject shield;
+
+    bool resting;
+
+    void Start()
+    {
+        
+        // Getting Objects and setting certain constraints
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        rigidBody = GetComponentInChildren<Rigidbody>();
+        capsule = GetComponentInChildren<CapsuleCollider>();
+
+        capsuleHeight = capsule.height;
+        capsuleCenter = capsule.center;
+
+        agent.stoppingDistance = 4;
+
+        findObject();
+        setObjectStateOff();
+
+        characterInventory = 8;
+        resting = false;
+    }
+
+    void Update()
+    {
+        CharacterStates();
+    }
+
+    public void CharacterStates()
+    {
+        CheckLastState();
+
+        switch (currentState)
+        {
+            case CharacterState.idle:
+                GameObject target = findNearestHarvestableObject();
+                setTarget(target.transform.position);
+                currentState = CharacterState.walkingToJob;
+                break;
+            
+            case CharacterState.walkingToJob:
+                if (agent.pathPending)
+                {
+                    return;
+                }
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {   
+                    Collider[] cols = Physics.OverlapSphere(agent.destination, 3);
+                    
+                    foreach (Collider c in cols)
+                    {
+                        interactableObject = c.GetComponent<IInteractable>();
+
+                        if (interactableObject != null)
+                        {
+                            interactableObject.interact(this);
+
+                            transform.LookAt(c.transform);
+
+                            currentState = CharacterState.DoingJob;
+
+                            switch (resourceType)
+                            {
+                                case ResourceManager.Resource.Stone:
+                                    animator.SetBool("isMining", true);
+                                    pickaxe.SetActive(true);
+                                    break;
+                                case ResourceManager.Resource.Wood:
+                                    animator.SetBool("isMining", true);
+                                    pickaxe.SetActive(true);
+                                    break;
+                                case ResourceManager.Resource.Food:
+                                    animator.SetBool("isMining", true);
+                                    pickaxe.SetActive(true);
+                                    break;
+                                case ResourceManager.Resource.MeleeSkill:
+                                    animator.SetBool("isMining", true);
+                                    sword.SetActive(true);
+                                    shield.SetActive(true);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                break;
+
+            case CharacterState.DoingJob:
+
+                if (characterInventory >= characterInventoryMax)
+                {
+                    currentState = CharacterState.ReturningFromJob;
+                    switch (resourceType)
+                    {
+                        case ResourceManager.Resource.Stone:
+                            animator.SetBool("isMining", false);
+                            pickaxe.SetActive(false);
+                            setTarget(findNearestStorageItemObject().transform.position);
+                            break;
+                        case ResourceManager.Resource.Wood:
+                            animator.SetBool("isMining", false);
+                            pickaxe.SetActive(false);
+                            setTarget(findNearestStorageItemObject().transform.position);
+                            break;
+                        case ResourceManager.Resource.Food:
+                            animator.SetBool("isMining", false);
+                            pickaxe.SetActive(false);
+                            setTarget(findNearestStorageItemObject().transform.position);
+                            break;
+                        case ResourceManager.Resource.MeleeSkill:
+                            animator.SetBool("isMining", false);
+                            sword.SetActive(false);
+                            shield.SetActive(false);
+                            setTarget(findNearestStorageItemObject().transform.position);
+                            break;
+                    }
+
+                }
+                break;
+
+
+            case CharacterState.ReturningFromJob:
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    Collider[] cols = Physics.OverlapSphere(agent.destination, 3);
+
+                    foreach (Collider c in cols)
+                    {
+                        interactableObject = c.GetComponent<IInteractable>();
+
+                        if (interactableObject != null)
+                        {
+                            interactableObject.interact(this);
+
+                            transform.LookAt(c.transform);
+
+                            currentState = CharacterState.storing;
+                        }
+                    }
+                }
+
+                break;
+
+            case CharacterState.ReturningToRestPosition:
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    currentState = CharacterState.resting;
+                }
+                    break;
+
+            case CharacterState.storing:
+                storage.giveItem(resourceType, characterInventory);
+                // temp anim as is unused and will show it working
+                animator.SetBool("isWalkingBackward", true);
+                if (characterInventory == 0)
+                {
+                    currentState = CharacterState.ReturningToRestPosition;
+                    setTarget(new Vector3(-31, 0, -12));
+                }
+                break;
+
+            case CharacterState.resting:
+                if (!resting)
+                {
+                    rest(5);
+                    resting = true;
+                }
+                
+                break;
+        }
+    }
+
+    // Allows Camera to focus and follow this character
+    public void OnMouseDown()
+    {
+        CameraControl.instance.focusTrans = transform;
+    }
+
+    // Sets the agent target
+    public void setTarget(Vector3 position)
+    {
+        animator.SetBool("isWalkingForward", true);
+        agent.SetDestination(position);
+    }
+
+    internal CharacterState getState()
+    {
+        return currentState;
+    }
+
+    internal void CheckLastState()
+    {
+        if (lastState == currentState)
+        {
+            return;
+        }
+
+        switch (lastState)
+        {
+            case CharacterState.idle:
+                // Not currently interacting with anything
+                if (interactableObject != null)
+                {
+                    interactableObject = null;
+                }
+
+                // Disables all tools/weapons
+                setObjectStateOff();
+
+                // Selecting a new task now that they're idle
+                //SelectTask();
+
+                break;
+
+            case CharacterState.walkingToJob:
+                // Clears any previous interactable objects
+                if (interactableObject != null)
+                {
+                    interactableObject = null;
+                }
+
+                // Disables all tools/weapons
+                setObjectStateOff();
+
+                // Disables any animations it shouldn't have
+                animator.SetBool("isWalkingForward", false);
+                break;
+
+            case CharacterState.ReturningToRestPosition:
+                animator.SetBool("isWalkingForward", false);
+                break;
+
+            case CharacterState.mining:
+                animator.SetBool("isMining", false);
+                break;
+
+            case CharacterState.practice:
+                //animator.SetBool("isAttackingMelee", false);
+                animator.SetBool("isMining", false);
+                break;
+
+            case CharacterState.storing:
+                animator.SetBool("isWalkingBackward", false);
+                break;
+
+            case CharacterState.reading:
+                //animator.SetBool("isLearning", false);
+                break;
+
+            case CharacterState.resting:
+                
+                break;
+
+            case CharacterState.ReturningFromJob:
+                animator.SetBool("isWalkingForward", false);
+                pickaxe.SetActive(false);
+                break;
+        }
+
+        lastState = currentState;
+    }
+
+    internal void IAmHarvestable(HarvestableObject harvestableObject)
+    {
+        harvest = harvestableObject;
+        resourceType = harvest.getResourceType();
+    }
+
+    internal void IAmStorage(StorageItem storage)
+    {
+        this.storage = storage;
+    }
+
+    internal void give(float amount)
+    {
+        if (characterInventory >= characterInventoryMax)
+        {
+            harvest.BreakCoroutine();
+            return;
+        }
+
+        characterInventory += amount;
+        Debug.Log(characterInventory);
+    }
+
+    internal ResourceManager.Resource getResourceType()
+    {
+        return resourceType;
+    }
+
+    internal float getInventory()
+    {
+        return characterInventory;
+    }
+
+    internal float getMaxInventory()
+    {
+        return characterInventoryMax;
+    }
+
+    internal void setHarvestObjectNull()
+    {
+        if (harvest != null)
+        {
+            harvest = null;
+        }
+    }
+
+    internal void setStorageObjectNull()
+    {
+        if (storage != null)
+        {
+            storage = null;
+        }
+    }
+
+    internal void removeFromInventory(float rate)
+    {
+        characterInventory -= rate;
+        Debug.Log("Inventory amount: " + characterInventory);
+    }
+
+    private void findObject()
+    {
+        pickaxe = GameObject.Find("Pickaxe");
+        sword = GameObject.Find("Sword");
+        shield = GameObject.Find("Shield");
+    }
+
+    private void setObjectStateOff()
+    {
+        pickaxe.SetActive(false);
+        sword.SetActive(false);
+        shield.SetActive(false);
+    }
+
+    private GameObject findNearestHarvestableObject()
+    {
+        GameObject nearest = null; 
+        foreach(GameObject g in ResourceManager.instance.getHarvestableObjectsList())
+        {
+            HarvestableObject ho = g.GetComponent<HarvestableObject>();
+            if (nearest == null)
+            {
+                nearest = g;
+            }
+            if (Vector3.Distance(transform.position, g.transform.position) < Vector3.Distance(transform.position, nearest.transform.position) && ResourceManager.instance.getJobResource(job) == ho.GetResource())
+            {
+                nearest = g;
+            }
+        }
+        return nearest;
+    }
+
+    private GameObject findNearestStorageItemObject()
+    {
+        GameObject nearest = null;
+        foreach (GameObject g in ResourceManager.instance.getStorageItemsList())
+        {
+            if (nearest == null)
+            {
+                nearest = g;
+            }
+            if (Vector3.Distance(transform.position, g.transform.position) < Vector3.Distance(transform.position, nearest.transform.position))
+            {
+                nearest = g;
+            }
+        }
+
+        return nearest;
+    }
+
+    private void rest(float time)
+    {
+        StartCoroutine(restIE(time));
+    }
+
+    private IEnumerator restIE(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Rested");
+        currentState = CharacterState.idle;
+        resting = false;
+    }
+
+}
---
+++ Unity Files/JPPG/Assets/Scripts/Game/AI/ForestCreature.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForestCreature : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent agent;
+
+    private Vector2 minPos = new Vector2(-380f, -210f);
+    private Vector2 maxPos = new Vector2(140f, 75f);
+    private Vector2 minTownBounds = new Vector2(-150f, -85f);
+    private Vector2 maxTownBounds = new Vector2(20f, 15f);
+
+    bool isSprinting = false;
+    bool isWaiting = false;
+
+    bool hasTarget = false;
+    Vector3 target;
+    float check = 5f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 4;
+        setTarget(moveTarget(out target));
+    }
+
+    void Update()
+    {
+        check -= Time.deltaTime;
+
+        if (check <= 0)
+        {
+            checkNotInTown();
+            check = 5f;
+        }
+
+        if (hasTarget)
+        {
+            if (agent.pathPending)
+            {
+                return;
+            }
+            if (agent.remainingDistance < agent.stoppingDistance)
+            {
+                hasTarget = false;
+                isWaiting = true;
+                wait(5);
+            }
+        }
+        if (!isWaiting && !hasTarget)
+        {   
+            setTarget(moveTarget(out target));
+        }
+    }
+
+    internal void setTarget(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+
+    private Vector3 moveTarget(out Vector3 target)
+    {
+        float x = Random.Range(minPos.x, maxPos.x);
+        float z = Random.Range(minPos.y, maxPos.y);
+
+        Ray ray = new Ray(new Vector3(x, 1000, z), Vector3.down);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        Vector3 pos = new Vector3(x, hit.point.y, z);
+
+        if (x < maxTownBounds.x && x > minTownBounds.x && z < maxTownBounds.y && z > minTownBounds.y)
+        {
+            return moveTarget(out target);
+        }
+
+        target = pos;
+        hasTarget = true;
+        return pos;
+    }
+
+    internal void toggleSprint()
+    {
+        if (isSprinting)
+        {
+            isSprinting = false;
+        }
+        else
+        {
+            isSprinting = true;
+        }
+    }
+
+    private void wait(float time)
+    {
+        StartCoroutine(waitIE(time));
+    }
+
+    private IEnumerator waitIE(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Waited");
+        isWaiting = false;
+    }
+
+    void checkNotInTown()
+    {
+        if (transform.position.x < maxTownBounds.x && transform.position.x > minTownBounds.x &&
+            transform.position.z < maxTownBounds.y && transform.position.z > minTownBounds.y)
+        {
+            setTarget(moveTarget(out target));
+        }
+    }
+}
